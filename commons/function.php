db84086738deecff057e50a443a405be36290d59
lam4999 <lamdtph49959@gmail.com>
2025-06-02 19:56:02 +0700

    lamcommit

---
+++ commons/function.php
@@ -0,0 +1,58 @@
+<?php
+
+// Kết nối CSDL qua PDO
+function connectDB() {
+    // Kết nối CSDL
+    $host = DB_HOST;
+    $port = DB_PORT;
+    $dbname = DB_NAME;
+
+    try {
+        $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", DB_USERNAME, DB_PASSWORD);
+
+        // cài đặt chế độ báo lỗi là xử lý ngoại lệ
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // cài đặt chế độ trả dữ liệu
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    
+        return $conn;
+    } catch (PDOException $e) {
+        echo ("Connection failed: " . $e->getMessage());
+    }
+}
+
+
+ function upLoadFile($file, $folderUpload) {
+    $pathStorage = $folderUpload . time() . $file['name'];
+
+    $from = $file['tmp_name'];
+    $to = PATH_ROOT . $pathStorage;
+
+    if(move_uploaded_file($from,$to)){
+        return $pathStorage;
+    }
+    return null;
+}
+
+function deleteFile($file){
+    $pathDelete = PATH_ROOT . $file;
+    if(file_exists($pathDelete)){
+        unlink($pathDelete);
+    }
+}
+
+function deleteSessionError(){
+    if(isset($_SESSION['flash'])){
+        unset($_SESSION['flash']);
+    }
+ }
+
+ if (isset($_SESSION['flash_message'])) {
+    echo '<div class="bg-green-100 text-green-700 border border-green-400 p-4 rounded-lg shadow-md mb-4">' . $_SESSION['flash_message'] . '</div>';
+    unset($_SESSION['flash_message']); // Xóa thông báo sau khi hiển thị
+}
+
+
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+
--- index.php
+++ index.php
@@ -1,18 +1,1 @@
-<?php
-session_start();
-include 'app/database/database.php';
-
-
-
-include 'app/models/admin/Homemodel.php';
-include 'app/models/admin/UserModel.php';
-
-
-include 'app/controllers/admin/ControllerAdmin.php';
-include 'app/controllers/admin/Homecontroller.php';
-include 'app/controllers/admin/LoginController.php';
-include 'app/controllers/admin/UserController.php';
-const BASE_URL = "http://localhost/project1_2025/";
-include 'router/web.php';
-
-// echo password_hash('123456', PASSWORD_BCRYPT);
+abc
